daf7881dae777b021d1f8f40549cd2dde25f8e2b
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-10-16 13:57:17 +0200

    framer motion works

--- app/routes/home/route.tsx
+++ app/routes/home/route.tsx
@@ -1,7 +1,15 @@
+import { motion } from 'framer-motion'
+
 export default function Projects() {
   return (
     <div className='h-full'>
       <h1>HELLO HOME</h1>
+      <motion.div
+        className='h-9 w-10 bg-blue-500'
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+      />
     </div>
   )
 }
