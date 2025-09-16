3f093268138a6ab4118838fccf1c3cb03dec50bc
arebrinkemil <earebrink@gmail.com>
2025-09-16 20:33:35 +0200

    test

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,19 +1,17 @@
-import { Divider } from '@nextui-org/react'
+import { Divider } from "@nextui-org/react";
 
 export const Footer = () => {
   return (
     <>
-      <div className='text-silver bg-darkgray mt-8 flex w-full flex-row py-4'>
-        <ul className='flex w-full flex-col justify-center gap-2 px-4 lg:flex-row lg:gap-4 lg:py-4'>
+      <div className="text-silver bg-darkgray mt-8 flex w-full flex-row py-4">
+        <ul className="flex w-full flex-col justify-center gap-2 px-4 lg:flex-row lg:gap-4 lg:py-4">
           <li>Made in REMIX</li>
 
           <li>By Emil Årebrink</li>
-
-          <li>YRGO 2024</li>
 
           <li>Contact: earebrink@gmail.com</li>
         </ul>
       </div>
     </>
-  )
-}
+  );
+};
